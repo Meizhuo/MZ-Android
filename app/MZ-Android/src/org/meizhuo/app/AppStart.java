585cfba36793c002b7a421972cecad1f3c08fd7e
Jayin Ton <tonjayin@gmail.com>
2014-08-27 11:08:15 +0800

    jar lib

--- app/MZ-Android/src/org/meizhuo/app/AppStart.java
+++ app/MZ-Android/src/org/meizhuo/app/AppStart.java
@@ -1,7 +1,22 @@
 package org.meizhuo.app;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class AppStart extends Activity {
-	//TODO  注册改Activity
+ 
+	@InjectView(R.id.textView1)
+	TextView tv;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.acty_appstart);
+		ButterKnife.inject(this);
+		tv.setText("ButterKnife!");
+		
+	}
 }
