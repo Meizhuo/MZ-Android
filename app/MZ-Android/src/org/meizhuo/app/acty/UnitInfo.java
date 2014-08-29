6d547f08f80486c198b50843d8a121d32f0f95f2
Jayin Ton <tonjayin@gmail.com>
2014-08-29 16:49:35 +0800

    step 1 for unitinfo

--- app/MZ-Android/src/org/meizhuo/app/acty/UnitInfo.java
+++ app/MZ-Android/src/org/meizhuo/app/acty/UnitInfo.java
@@ -1,16 +1,26 @@
 package org.meizhuo.app.acty;
 
+import org.meizhuo.adapter.UnitinfoAdapter;
 import org.meizhuo.app.BaseActivity;
 import org.meizhuo.app.R;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
 /**
  * 单位信息列表
  * @author Jayin
  *
  */
 public class UnitInfo extends BaseActivity {
+	@InjectView(R.id.tv_title) TextView tv_title; 
+	@InjectView(R.id.lv_unitinfo) ListView lv;
+	
+	UnitinfoAdapter adatper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +28,21 @@ public class UnitInfo extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.acty_unitinfo);
 		ButterKnife.inject(this);
+		
+		tv_title.setText("单位信息");
+		
+		String[] names = new String[]{"基本职能","职业训练法律法规","职业训练通知"};
+		adatper = new UnitinfoAdapter(this, names);
+		lv.setAdapter(adatper);
+	}
+	
+	@OnClick(R.id.btn_title_back)
+	public void back(){
+		closeActivity();
+	}
+	
+	@OnItemClick(R.id.lv_unitinfo)
+	public void item_click(int position){
+		toast(""+position);
 	}
 }
