0f1716a4b780c015387d80bed4a907739568b1f4
Jayin Ton <tonjayin@gmail.com>
2014-08-29 14:27:54 +0800

    更换头部UI

--- app/MZ-Android/src/org/meizhuo/app/acty/Main.java
+++ app/MZ-Android/src/org/meizhuo/app/acty/Main.java
@@ -3,11 +3,16 @@ package org.meizhuo.app.acty;
 import org.meizhuo.app.BaseActivity;
 import org.meizhuo.app.R;
 
-import butterknife.ButterKnife;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class Main extends BaseActivity {
+	
+	@InjectView(R.id.ic_title_back) ImageView iv_title_back;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,40 @@ public class Main extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.acty_main);
 		ButterKnife.inject(this);
+		iv_title_back.setVisibility(View.INVISIBLE);
 	}
+	
+	@OnClick(R.id.btn_unitinfo)
+	public void unitinfo(){
+		openActivity(UnitInfo.class);
+	}
+	
+	@OnClick(R.id.btn_institutions)
+	public void Institutions(){
+		openActivity(Institutions.class);
+	}
+	
+	@OnClick(R.id.btn_institution_consult)
+	public void institution_consult(){
+		openActivity(InstitutionConsult.class);
+	}
+	
+	@OnClick(R.id.btn_public_consult)
+	public void public_consult(){
+		openActivity(PublicConsult.class);
+	}
+	
+	
+	@OnClick(R.id.btn_usercenter)
+	public void usercenter(){
+		openActivity(UserCenter.class);
+	}
+	
+	@OnClick(R.id.btn_setting)
+	public void setting(){
+		openActivity(Setting.class);
+	}
+	
+	
 
 }
