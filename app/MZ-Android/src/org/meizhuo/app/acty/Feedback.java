3fc35e8af38cb7ec5fd2d2da0c7a92a5a2f8d4d3
jason <zyt769562065@163.com>
2014-08-29 20:42:31 +0800

    完成了反馈功能

--- app/MZ-Android/src/org/meizhuo/app/acty/Feedback.java
+++ app/MZ-Android/src/org/meizhuo/app/acty/Feedback.java
@@ -4,6 +4,7 @@ import org.meizhuo.app.BaseActivity;
 import org.meizhuo.app.R;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,7 +13,12 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 
 public class Feedback extends BaseActivity {
-	@InjectView(R.id.back) Button back;
+	
+	
+	@InjectView(R.id.btn_confirm) View confirm;
+	@InjectView(R.id.btn_cancel) View cancel;
+	@InjectView(R.id.feedback_et) EditText et;
+	@InjectView(R.id.feedback_back) Button back;
  	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,27 @@ public class Feedback extends BaseActivity {
 		setContentView(R.layout.acty_feedback);
 		ButterKnife.inject(this);
 		
+	}
+	
+	
+	@OnClick(R.id.feedback_back)
+	public void back()
+	{
+		closeActivity();
+	}
+	@OnClick(R.id.btn_confirm)
+	public void confirm()
+	{
+		Toast.makeText(Feedback.this, "反馈已发送", Toast.LENGTH_SHORT).show();
 		
 	}
+	@OnClick(R.id.btn_cancel)
+	public void cancel()
+	{
+		et.setText("");
+		
+	}
+
+	
 	
 }
