6da275caab6b3486140d5db31235796691078697
Jayin Ton <tonjayin@gmail.com>
2014-09-01 19:10:15 +0800

    add lib PagerSlidingTabStrip

---
+++ app/MZ-Android/src/org/meizhuo/fragment/UnitInfoLastest.java
@@ -0,0 +1,18 @@
+package org.meizhuo.fragment;
+
+import org.meizhuo.app.R;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UnitInfoLastest extends BaseFragment {
+
+	
+	@Override public View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState,R.layout.fm_unitinfolastest);
+		return contentView;
+	}
+}
---
+++ app/MZ-Android/src/org/meizhuo/fragment/UnitInfoPolicy.java
@@ -0,0 +1,18 @@
+package org.meizhuo.fragment;
+
+import org.meizhuo.app.R;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UnitInfoPolicy extends BaseFragment {
+	
+	@Override public View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState,R.layout.fm_unitinfopolicy);
+		return contentView;
+	}
+
+}
