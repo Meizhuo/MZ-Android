a5c3dfd1eda4721a92f311ade30accc938da6b0a
jason <zyt769562065@163.com>
2014-10-07 22:41:04 +0800

    Fix #44:法律法规栏目 三个tab互动的时候有bug!想办法解决一下

--- app/MZ-Android/src/org/meizhuo/app/acty/Professional_Training.java
+++ app/MZ-Android/src/org/meizhuo/app/acty/Professional_Training.java
@@ -4,22 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.meizhuo.adapter.ImagePagerAdapter;
-import org.meizhuo.adapter.UnitinfoAdapter;
 import org.meizhuo.app.BaseActivity;
 import org.meizhuo.app.R;
-import org.meizhuo.fragment.UnitInfoBase;
-import org.meizhuo.fragment.UnitInfoLastest;
-import org.meizhuo.fragment.UnitInfoPolicy;
+import org.meizhuo.fragment.Professional_Training_Base;
+import org.meizhuo.fragment.Professional_Training_AboutNews;
+import org.meizhuo.fragment.Professional_Training_Notification;
+import org.meizhuo.fragment.Professional_Training_Policy;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.View;
-import android.widget.ImageView;
 import butterknife.InjectView;
-import butterknife.OnItemClick;
 
 /**
  * 单位信息列表
@@ -28,21 +25,22 @@ import butterknife.OnItemClick;
  * 
  */
 public class Professional_Training extends BaseActivity {
+	private static final String TAG = "Professional_Training";
+	
 	@InjectView(R.id.tabs) com.astuetz.PagerSlidingTabStrip mPagerSlidingTabStrip;
 	@InjectView(R.id.viewpager) ViewPager mViewPager;
-	UnitinfoAdapter adatper_unitinfo;
 	ImagePagerAdapter adapter_imagepage;
-
+ 
 	List<Fragment> fragments = new ArrayList<Fragment>();
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState, R.layout.acty_unitinfo);
+		super.onCreate(savedInstanceState, R.layout.acty_unitinfo );
 		setAppTitle("职业培训信息");
 
-		fragments.add(new UnitInfoLastest());
-		fragments.add(new UnitInfoBase());
-		fragments.add(new UnitInfoPolicy());
+		fragments.add(new Professional_Training_AboutNews());
+		fragments.add(new Professional_Training_Base());
+		fragments.add(new Professional_Training_Policy());
 
 		mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(),
 				fragments));
@@ -55,7 +53,7 @@ public class Professional_Training extends BaseActivity {
 
 		List<Fragment> fragments;
 
-		private final String[] TITLES = {  "通知" ,"基本职能", "法律法规"};
+		private final String[] TITLES = {  "相关新闻","基本职能","法规政策" };
 
 		public MyPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
 			super(fm);
