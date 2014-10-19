8cad820990e7f2367a618f3234eba4fe90394d8f
jason <zyt769562065@163.com>
2014-10-19 13:50:23 +0800

    文章标题修正

--- app/MZ-Android/src/org/meizhuo/app/acty/Professional_Article.java
+++ app/MZ-Android/src/org/meizhuo/app/acty/Professional_Article.java
@@ -2,20 +2,20 @@ package org.meizhuo.app.acty;
 
 import java.util.List;
 
-import org.apache.http.Header;
-import org.json.JSONObject;
-import org.meizhuo.api.ArticleAPI;
 import org.meizhuo.api.RestClient;
 import org.meizhuo.app.BaseActivity;
 import org.meizhuo.app.R;
-import org.meizhuo.imple.JsonResponseHandler;
 
+
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
+import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import butterknife.InjectView;
 
 /**
@@ -24,6 +24,7 @@ import butterknife.InjectView;
  * @author Jayin
  * 
  */
+@SuppressLint("SetJavaScriptEnabled")
 public class Professional_Article extends BaseActivity  {
 	
 	private static final String TAG = "Professional_Article";
@@ -34,11 +35,11 @@ public class Professional_Article extends BaseActivity  {
 	String url;
 	String doc_id;
 
+	  @SuppressLint("SetJavaScriptEnabled")  
 	@Override protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState, R.layout.acty_professional_artilecontent);
-		
-		
+	
 		initData();
 		initLayout();
 		setAppTitle(title);
@@ -54,13 +55,91 @@ public class Professional_Article extends BaseActivity  {
 		String doc_id = intent.getStringExtra("doc_id");
 		title  = intent.getStringExtra("title");
 		String baseurl = RestClient.BASE_URL;
+		
 		url = baseurl + "/admin/index/viewDocument" + "/" + "doc_id" + "/" + doc_id;
+		
 	}
 	
 	private void initLayout(){
+		webview.getSettings().setJavaScriptEnabled(true);
+	
 		webview.loadUrl(url);
-		
+		webview.setWebViewClient(new MyWebViewClient());
+		//添加js交互接口类
+		webview.addJavascriptInterface(new JavascriptInterface(this), "imagelistner");
 	}
+	
+	private void addImageClickListner() {
+		webview.loadUrl("javascript:(function(){" +
+		"var objs = document.getElementsByTagName(\"img\"); " + 
+				"for(var i=0;i<objs.length;i++)  " + 
+		"{"
+				+ "    objs[i].onclick=function()  " + 
+		"    {  " 
+				+ "        window.imagelistner.openImage(this.src);  " + 
+		"    }  " + 
+		"}" + 
+		"})()");
+	}
+	
+	/**
+	 * js通信接口类
+	 *
+	 */
+	public class JavascriptInterface {
+		private Context context;
+		
+		public JavascriptInterface(Context context) {
+			// TODO Auto-generated constructor stub
+			this.context = context;
+		}
+		
+		@android.webkit.JavascriptInterface
+		public void openImage(String img) {
+			Log.i(TAG, "执行了");
+			
+			Intent intent = new Intent();
+			intent.putExtra("image", img);
+			intent.setClass(context, ShowWebImageActivity.class);
+			context.startActivity(intent);
+		}
+	}
+	
+	private class MyWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			// TODO Auto-generated method stub
+			return super.shouldOverrideUrlLoading(view, url);
+		}
+		
+		@SuppressLint("SetJavaScriptEnabled")
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			// TODO Auto-generated method stub
+			view.getSettings().setJavaScriptEnabled(true);
+			super.onPageFinished(view, url);
+			//html加载完成之后,添加监听图片的点击js函数
+			addImageClickListner();
+		}
+		
+		@SuppressLint("SetJavaScriptEnabled")
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			// TODO Auto-generated method stub
+			view.getSettings().setJavaScriptEnabled(true);
+			super.onPageStarted(view, url, favicon);
+		}
+		
+		@Override
+		public void onReceivedError(WebView view, int errorCode,
+				String description, String failingUrl) {
+			// TODO Auto-generated method stub
+			
+			super.onReceivedError(view, errorCode, description, failingUrl);
+		}
+	}
+	
+
 	
 
 
